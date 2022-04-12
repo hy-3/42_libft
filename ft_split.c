36eec3d8800c5b12d5c49284c2411c4fe2e498ff
Hiroki Yamamoto <hiyamamo@student.42madrid.com>
2022-04-12 15:44:02 +0200

    fix ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -1,80 +1,67 @@
+#include <stddef.h>
 #include <stdlib.h>
 
 int	count_num_of_strings(char const *s, char c)
 {
 	int	i;
 	int	res;
-	int	flag;
 
 	i = 0;
 	res = 0;
 	while (s[i] != '\0')
 	{
-		flag = 0;
 		if (s[i] != c)
 		{
 			res++;
-			flag = 1;
-			while (s[i++] != c)
-			{
-				if (s[i] == '\0')
-					break ;
+			while (s[i++] != c  && s[i] != '\0')
 				continue ;
-			}
 		}
-		if (flag == 0)
+		else
 			i++;
 	}
 	return (res);
 }
 
 // TODO implement Free
-char	**ft_split(char const *s, char c)
+void	fill_string_in_array(char const *s, char c, char **res)
 {
-	int	num_of_strings;
-	char	**res;
 	int	i;
 	int	k;
-	int	start;
-	int	end;
 	int	l;
-	int	flag;
+	int	start;
 
-	num_of_strings = count_num_of_strings(s, c);
-	res = malloc(num_of_strings * sizeof(char *));
-	if (res == NULL)
-		return (NULL);
 	i = 0;
 	k = 0;
 	while (s[i] != '\0')
 	{
-		flag = 0;
 		if (s[i] != c)
 		{
-			flag = 1;
 			start = i;
-			while (s[i++] != c)
-			{
-				if (s[i] == '\0')
-					break ;
+			while (s[i++] != c && s[i] != '\0')
 				continue ;
-			}
-			end = i;
-			res[k] = malloc((end - start + 1) * sizeof(char));
+			res[k] = malloc((i - start + 1) * sizeof(char));
 			// if (res[k] == NULL)
-			// 	free();
+			// 	free(res[k]);
 			l = 0;
-			while ((end - start) > 0)
-			{
-				res[k][l] = s[start];
-				start++;
-				l++;
-			}
-			res[k][l] = '\0';
-			k++;
+			while ((i - start) > 0)
+				res[k][l++] = s[start++];
+			res[k++][l] = '\0';
 		}
-		if (flag == 0)
+		else
 			i++;
 	}
+	res[k] = NULL;
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int	num_of_strings;
+	char	**res;
+
+	num_of_strings = count_num_of_strings(s, c);
+	res = malloc((num_of_strings + 1) * sizeof(char *));
+	if (res == NULL)
+		return (NULL);
+	fill_string_in_array(s, c, res);
 	return (res);
 }

615f8d157639d8e92d2df78dd39f105cd185928f
Hiroki Yamamoto <hymt73@gmail.com>
2022-04-21 15:02:00 +0200

    fixed datatype to handle int_max & when the number is greater than lang_max/min

--- ft_atoi.c
+++ ft_atoi.c
@@ -11,32 +11,30 @@
 /* ************************************************************************** */
 
 #include <stddef.h>
-
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+#include <limits.h>
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	res;
-	int	sign;
+	int		i;
+	int		sign;
+	size_t	res;
 
 	i = 0;
 	sign = 1;
 	res = 0;
-	if (ft_strncmp("-99999999999999999999999999", str, 30) == 0)
-		return (0);
-	if (ft_strncmp("99999999999999999999999999", str, 30) == 0)
-		return (-1);
 	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' \
 			|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
 		i++;
 	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
+		if (str[i++] == '-')
 			sign = -1;
-		i++;
-	}
 	while ('0' <= str[i] && str[i] <= '9' && str[i] != '\0')
+	{
+		if (res > LONG_MAX / 10 && sign < 0)
+			return (0);
+		if (res > LONG_MAX / 10 && sign > 0)
+			return (-1);
 		res = res * 10 + str[i++] - '0';
-	return (res * sign);
+	}
+	return (int)(res * sign);
 }
